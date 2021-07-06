b39632127707dd5ed2e56274fd578d4a79822530
Bean <beansobre@gmail.com>
2021-07-07 06:55:39 +0800

    added kata bugsFinder and ShoppingCart

---
+++ 132-Bugs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _132_Bugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HowManyDalmatians(20));
+        }
+
+        public static string HowManyDalmatians(int number)
+        {
+            List<string> dogs = new List<string>()
+            {
+                "Hardly any",
+                "More than a handful!",
+                "Woah that's a lot of dogs!",
+                "101 DALMATIONS!!!"
+            };
+            string respond = ( number <= 10) ? dogs[0] : number <= 50 ? dogs[1] : number == 101 ? dogs[3] : dogs[2];
+          
+            return respond;
+        }
+    }
+}
---
+++ 133-ShoppingCart/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _133_ShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+     public static List<(string, string, string)> ShoppingCalculation(List<string> input) {
+        throw new NotImplementedException();
+    }
+    }
+}
