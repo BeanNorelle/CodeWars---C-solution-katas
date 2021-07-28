1cd466d93df7752ff28943ff2be0a33d682d6893
Bean <beansobre@gmail.com>
2021-07-28 22:53:42 +0800

    add DirectionReduction/helpYourGranny

---
+++ 165-DirectionReductions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _165_DirectionReductions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
