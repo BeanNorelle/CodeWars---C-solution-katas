dcc6b77042ab7200469603b2df8473b2b5a8f86c
Bean <beansobre@gmail.com>
2021-08-05 14:22:49 +0800

    Add 181-182

---
+++ 181-FormatWordsToSentence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _181_FormatWordsToSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] words = {"Kevin","Kayn"};
+            Console.WriteLine(FormatWords(words));
+        }
+
+        public static string FormatWords(string [] wordsArr)
+        {
+           var words = FilterEmpty(wordsArr);
+
+          if(words.Length==0) return "";
+          
+          if(words.Length==1) return words[0];
+
+          if(words.Length==2) return words[0] + " and " + words[1];
+         
+
+          var s = string.Join(", ", words.SkipLast(1)) + " and " + words.Last();
+
+          return s;
+            
+        }
+
+        public static string [] FilterEmpty(string[] words)
+        {
+          List <string> list = new List<string>();
+          foreach(string word in words)
+          {
+              if(word!=string.Empty) list.Add(word);
+          }
+          return list.ToArray();
+        }
+    }
+}
---
+++ 182-ZeroFill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _182_ZeroFill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ZeroFill(11,5));
+        }
+
+        public static string ZeroFill(int number, int size)
+        {
+           number = (number<0) ? number*-1 : number; 
+           string num = number.ToString();
+          
+           int nlength = num.Length;
+           if(nlength>size) return num;
+           
+           int adds = Math.Abs(nlength - size);
+
+           return new string('0',adds) + num; 
+        }
+    }
+}
