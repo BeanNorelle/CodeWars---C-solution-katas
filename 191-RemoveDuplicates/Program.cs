82c803190db14d2edeab3870a03871562ac02293
Bean <beansobre@gmail.com>
2021-08-13 15:27:37 +0800

    asdsad

---
+++ 191-RemoveDuplicates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _191_RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intarr = new List <int>{1, 5, 2, 0, 2, -3, 1, 10};
+            Unique(intarr).ForEach(x=>Console.WriteLine(x));
+        }
+
+         public static List<int> Unique(List<int> integers)
+        {
+           return integers.Distinct().ToList();
+        }
+    }
+}
---
+++ 192-BattleOfChar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _192_BattleOfChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Battle("ABC","Z"));
+        }
+        public static string Battle(string x, string y)
+        {
+           int xs = GetScore(x);
+           int ys = GetScore(y);
+
+           if(xs>ys) return x;
+           if(ys>xs) return y;
+
+           return "Tie!";
+        }
+        public static int GetScore(string str)
+        {  
+            int sum = 0;
+            foreach(char ch in str)
+            {
+                sum += Convert.ToInt32(ch)-64;
+            }
+            return sum;
+        }
+    }
+}
