6ade417cba6bfb6e8b64a7bcbdf29ddd2c961ad2
Bean <beansobre@gmail.com>
2021-08-23 09:57:07 +0800

    1 kata

---
+++ 202-TrainingTakeTenMin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _202_TrainingTakeTenMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] walk =  {"w","e","w","e","w","e","w","e","w","e","w","e"};
+            Console.WriteLine(IsValidWalk(walk));
+        }
+
+        public static bool IsValidWalk(string[] walk)
+        {
+                        if(walk.Length>10) return false;
+            int vert = 0;
+            int horz = 0;
+            foreach(var direction in walk)
+            {
+                switch(direction)
+                {
+                    case "n": vert++; break;
+                    case "s": vert--; break;
+                    case "w": horz++; break;
+                    case "e": horz--; break;
+                }
+            }
+            return (vert==0&&horz==0);
+        }
+    }
+}
