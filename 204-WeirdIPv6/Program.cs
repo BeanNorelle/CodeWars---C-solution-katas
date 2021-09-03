23092601910407bf79f8e6ec4124219d536ffdec
Bean <beansobre@gmail.com>
2021-09-03 09:40:49 +0800

    add weirdIPv6

---
+++ 204-WeirdIPv6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _204_WeirdIPv6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(ParseIPv6("1111:1111:1111:1111:1111:1111:1111:1111"));
+        }
+
+        public static string ParseIPv6(string iPv6)
+        {
+            string validhex = "1234567890ABCDEF";
+            List<string> Blocks = new List<string>();
+            string sb = string.Empty;
+            int sum = 0;
+            for(int i=0;i<iPv6.Length;i++)
+            {
+                if(validhex.Contains(iPv6[i]))
+                {
+                   sum += validhex.IndexOf(iPv6[i]) + 1;
+                }
+                else 
+                {
+                   sb += Convert.ToString(sum);
+                   sum = 0;
+                }
+                if(i==iPv6.Length-1)
+                {
+                    sb+=sum;
+                }
+            }
+            return sb;
+           
+        }
+    }
+}
